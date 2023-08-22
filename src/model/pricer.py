bb52f79e347880d2d38069e16dfaa85fc9d40af6
Valentin Catherine <valentincthrn@gmail.com>
2023-08-22 11:23:13 -0300

    change structure for data prep to other file

--- src/model/pricer.py
+++ src/model/pricer.py
@@ -1,7 +1,10 @@
 import torch
 from torch_geometric.data import Data, Batch
 import pandas as pd
+import functools as ft
 
+
+from src.model.data_manage import df_prep, dataset_prep
 from src.configs import RunConfiguration
 from src.utils.db import DBInterface
 
@@ -13,15 +16,17 @@ class GraphStockPricer(torch.nn.Module):
         db: DBInterface,
     ):
         super(GraphStockPricer, self).__init__()
-        self.config_data_prep: dict = config.model
+        self.config_data_prep: dict = config.data_prep
         self.config_encoding: dict = config.encoding
         self.config_model: dict = config.model
         self.targets: list = config.targets_to_predict
         self.db: DBInterface = db
 
     def data_prep(self):
-        df = self.db.read_sql(query="SELECT * FROM stocks")
-        print(df)
+        data = df_prep(self.db, self.targets)
+
+        dataset = dataset_prep(data, self.config_data_prep)
+        return
 
     def train(self):
         return
