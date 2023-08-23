e652a55b9fc61ca7d947f6dd99e63b299d14bca6
Valentin Catherine <valentincthrn@gmail.com>
2023-08-22 21:58:58 -0300

    change simple

--- src/experiments/simple.py
+++ src/experiments/simple.py
@@ -16,16 +16,26 @@ class SimpleEXP(_BaseEXP):
     @property
     def _targets(self) -> typing.List[str]:
         return [
-            "RRRP3.SA",
-            "BPAC11.SA",
-            "ENEV3.SA",
-            "B3SA3.SA",
-            "PETR4.SA",
-            "GOLL4.SA",
-            "EZTC3.SA",
-            "PETZ3.SA",
-            "BBDC4.SA",
+            "NUBR33.SA",
             "ITUB4.SA",
+            "ABCB4.SA",
+            "RPAD5.SA",
+            "BRIV4.SA",
+            "BAZA3.SA",
+            "BMGB4.SA",
+            "BPAN4.SA",
+            "BGIP4.SA",
+            "BEES3.SA",
+            "BRSR6.SA",
+            "BBDC4.SA",
+            "BBAS3.SA",
+            "BSLI4.SA",
+            "BPAC11.S",
+            "INBR32.S",
+            "BMEB4.SA",
+            "BMIN4.SA",
+            "BNBR3.SA",
+            "SANB11.SA",
         ]
 
     @property
