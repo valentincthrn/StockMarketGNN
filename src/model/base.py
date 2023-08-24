f0aa9e7548d48a28468033ed12f8a52496fd3a68
Valentin Catherine <valentincthrn@gmail.com>
2023-08-23 22:53:35 -0300

    add exp model param

--- src/model/base.py
+++ src/model/base.py
@@ -11,8 +11,10 @@ import matplotlib.pyplot as plt
 import abc
 from random import shuffle
 from typing import List, Optional, Dict
+import collections
 
-from src.utils.common import mape_loss
+
+from src.utils.common import mape_loss, calculate_mape
 from src.model.data_manage import df_prep, dataset_prep
 from src.model.mygnn import GNN, testing
 from src.configs import RunConfiguration, DATE_FORMAT
@@ -41,10 +43,16 @@ class _BaseEXP(torch.nn.Module, abc.ABC):
         self.targets = self._targets
         self.features = self._features
         self.encoding_method = self._encodings
+        self.exp_model = self._exp_model
 
         self.db: DBInterface = db
 
+        assert (
+            self.data_config["history_length"] == self.model_config["history_length"]
+        ), "History Length of data ingestion should be equal"
+
         mlflow.log_param("Target Companies", self.targets)
+        mlflow.log_param("Nbr Companies", len(self.targets))
         mlflow.log_params(self.data_config)
         mlflow.log_params(self.model_config)
         mlflow.log_param("Encoding", self.encoding_method)
@@ -64,6 +72,11 @@ class _BaseEXP(torch.nn.Module, abc.ABC):
     def _features(self) -> Dict[str, bool]:
         raise NotImplementedError
 
+    @property
+    @abc.abstractmethod
+    def _exp_model(self) -> Dict[str, bool]:
+        raise NotImplementedError
+
     def data_prep(self):
         self.data_price, self.data_roc = df_prep(
             self.db, self.targets, price_to_roc=self.data_config["price_to_roc"]
@@ -74,14 +87,10 @@ class _BaseEXP(torch.nn.Module, abc.ABC):
         )
 
     def train(self):
-        self.model = GNN(
-            num_features=self.data_config["history_length"],
-            hidden_channels=self.model_config["hidden_size"],
-            num_classes=1,
-        )
+        self.model = GNN(model_config=self.model_config, exp_config=self.exp_model)
+        mlflow.log_params(self.exp_model)
 
-        data_price_true = self.data_price.copy()
-        data_price_true.columns = [col + "_true" for col in data_price_true.columns]
+        self.data_price.columns = [col + "_true" for col in self.data_price.columns]
 
         # Define a suitable optimizer
         optimizer = Adam(self.model.parameters(), lr=0.001)
@@ -113,96 +122,54 @@ class _BaseEXP(torch.nn.Module, abc.ABC):
                 self.model.eval()
             scheduler.step
 
-            print("Mean Training Loss: ", np.mean(loss_list))
-            mlflow.log_metric("Training Loss", np.mean(loss_list))
+            print("MAPE Training Loss: ", np.mean(loss_list))
+            mlflow.log_metric("MAPE Training Loss", np.mean(loss_list))
 
             testing(
                 self.model,
                 self.testingset,
-                data_price_true,
+                self.data_price,
                 self.data_config["test_days"],
+                during_training=True,
             )
 
     def evaluate(self):
-        # evaluate predictions
-        preds = []
-
-        # Iterate over each graph in the batch
-        for data in tqdm(self.testingset.to_data_list()):
-            out = list(self.model(data).detach().numpy())
-
-            preds.append(out)
-
-        pred_col_gnn = [col + "_pred_gnn" for col in self.data_price.columns]
-        self.result = pd.DataFrame(
-            np.squeeze(np.array(preds)),
-            columns=pred_col_gnn,
-            index=self.data_price.iloc[-self.data_config["test_days"] :].index,
+        self.result = testing(
+            self.model,
+            self.testingset,
+            self.data_price,
+            self.data_config["test_days"],
+            during_training=False,
         )
-
-        if self.data_config["price_to_roc"]:
-            first_price = pd.DataFrame(
-                data=self.data_price.iloc[-self.data_config["test_days"] - 1]
-            ).T
-            first_price.columns = self.result.columns
-
-            # GNN prediction
-            self.result = pd.concat(
-                [
-                    first_price,
-                    1 + self.result / 100,
-                ]
-            ).cumprod()
-
-        # Add the close price
-        self.result = self.result.join(self.data_price)
 
         # Add the naive predictions
-        naive_cols = [col + "_pred_naive" for col in self.data_price.columns]
-        self.result[naive_cols] = self.result[self.data_price.columns].shift(1)
+        pred_col_naive = [
+            col.replace("_true", "_pred_naive") for col in self.data_price.columns
+        ]
+
+        self.result[pred_col_naive] = self.result[self.data_price.columns].shift(1)
+
+        # Creating the MultiIndex
+        multi_columns = [
+            (col.split("_", 2)[1], col.split("_", 2)[2]) for col in self.result.columns
+        ]
+        multi_index = pd.MultiIndex.from_tuples(multi_columns)
+
+        # Set the columns of your dataframe to this MultiIndex
+        self.result.columns = multi_index
+        # Sort the columns based on the first level of the MultiIndex
+        self.result.sort_index(axis=1, level=0, inplace=True)
 
     def postprocess(self):
-        multi_index = []
-        for s in self.data_price.columns:
-            s_without_sa = s.lower().replace(".sa", "")
-            multi_index.append((s_without_sa, "pred_gnn"))
-        for s in self.data_price.columns:
-            s_without_sa = s.lower().replace(".sa", "")
-            multi_index.append((s_without_sa, "true"))
-        for s in self.data_price.columns:
-            s_without_sa = s.lower().replace(".sa", "")
-            multi_index.append((s_without_sa, "pred_naive"))
-        cols = pd.MultiIndex.from_tuples(multi_index)
-        self.result.columns = cols
+        for col in self.result.columns.get_level_values(0).unique():
+            true_values = self.result[col, "true"]
+            pred_naive_values = self.result[col, "pred_naive"]
+            naive_error = calculate_mape(true_values, pred_naive_values)
+            mlflow.log_metric(col + "_naive", naive_error)
 
-        symbol = []
-        rmse_gnn = []
-        rmse_naive = []
+        self.result.columns = [
+            " ".join(col).strip() for col in self.result.columns.values
+        ]
 
-        # outputing results
-        for s in self.data_price:
-            s_without_sa = s.lower().replace(".sa", "")
-            symbol.append(s_without_sa)
-            df_s = self.result[s_without_sa]
-            df_s.plot()
-            rmse_gnn.append(((df_s.true - df_s.pred_gnn) ** 2).mean() ** 0.5)
-            rmse_naive.append(((df_s.true - df_s.pred_naive) ** 2).mean() ** 0.5)
-
-        self.output = pd.DataFrame(
-            {
-                "symbol": symbol,
-                "rmse_gnn": rmse_gnn,
-                "rmse_naive": rmse_naive,
-            }
-        )
-
-        for symbol, rmse_gnn, rmse_naive in self.output.itertuples(index=False):
-            mlflow.log_metric(key=symbol + "_pred_test_gnn", value=rmse_gnn)
-            mlflow.log_metric(key=symbol + "_pred_test_naives", value=rmse_naive)
-
-        print(self.output)
-
-        return
-
-    def save(self):
-        self.result.to_csv("test.csv")
+        self.result.to_csv("output.csv")
+        mlflow.log_artifact("output.csv")
