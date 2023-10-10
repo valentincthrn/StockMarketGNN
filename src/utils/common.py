1a2ef03a5c51a01ca526da10dfe03cf691792f6f
Valentin Catherine <valentincthrn@gmail.com>
2023-10-10 16:41:54 -0300

    add positional encoding

--- src/utils/common.py
+++ src/utils/common.py
@@ -1,3 +1,4 @@
+import math
 import torch
 
 
@@ -12,3 +13,29 @@ def calculate_mape(true_values, pred_values):
     # Avoid division by zero
     mask = true_values != 0
     return 100 * (abs((true_values - pred_values) / true_values)[mask].mean())
+
+
+class PositionalEncoding(torch.nn.Module):
+    """Encoder that applies positional based encoding.
+    Encoder that considers data temporal position in the time series' tensor to provide
+    a encoding based on harmonic functions.
+    Attributes:
+    hidden_size (int): size of hidden representation
+    dropout (float): dropout rate
+    max_len (int): maximum quantity of features
+    """
+
+    def __init__(self, hidden_size, dropout=0.1, max_len=5000):
+        super().__init__()
+        self.dropout = torch.nn.Dropout(p=dropout)
+        self.hidden_size = hidden_size
+        self.div_term = torch.exp(
+            torch.arange(0, self.hidden_size, 2).float()
+            * (-math.log(10000.0) / self.hidden_size)
+        )
+
+    def forward(self, position):
+        pe = torch.empty(position.shape[0], self.hidden_size, device=position.device)
+        pe[:, 0::2] = torch.sin(position * self.div_term.to(position.device))
+        pe[:, 1::2] = torch.cos(position * self.div_term.to(position.device))
+        return self.dropout(pe)
