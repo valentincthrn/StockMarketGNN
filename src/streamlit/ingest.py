0759862e651fa8fb1aa534c764056c328c4a9554
Valentin Catherine <valentincthrn@gmail.com>
2023-11-19 23:40:52 -0300

    ingestion interface streamlit

---
+++ src/streamlit/ingest.py
@@ -0,0 +1,68 @@
+from pandas.tseries.offsets import BDay
+import streamlit as st
+import pandas as pd
+
+from src.utils.db import DBInterface
+from src.configs import DATE_FORMAT
+from datetime import datetime
+
+
+def extract_current_stocks_data():
+    
+    # Create a database instance
+    db = DBInterface()
+    
+    # Query to get the latest quote date for each stock
+    query_stocks = """
+            SELECT symbol, MAX(quote_date) as last_quote_date
+            FROM stocks
+            GROUP BY symbol
+            """
+
+    query_metadata = """
+            SELECT symbol, name
+            FROM stocks_metadata
+            """
+            
+    query_macro = """
+            SELECT indicators, MAX(quote_date) as last_quote_date
+            FROM macro
+            GROUP BY indicators
+            """
+            
+    df_stocks = db.read_sql(query=query_stocks)
+    df_metadata = db.read_sql(query=query_metadata)
+    df_macro = db.read_sql(query=query_macro)
+    
+    # Calculate the last business day
+    last_bday = (datetime.now() - BDay(1)).strftime(DATE_FORMAT)
+
+    # Check if each stock is up to date
+    df_stocks['is_prices_uptodate'] = df_stocks['last_quote_date'] >= last_bday
+    df_macro['is_prices_uptodate'] = df_macro['last_quote_date'] >= last_bday
+        
+    # get company name
+    df_stocks_name = (
+        df_stocks
+        .merge(
+            df_metadata,
+            on = "symbol",
+            how = "left"
+        )
+    )
+    
+    # Fill NaN
+    df_stocks_name["name"] = df_stocks_name["name"].fillna("Not Available")
+    
+    # Change date format
+    df_stocks_name['last_quote_date'] = pd.to_datetime(df_stocks_name['last_quote_date'])
+    df_stocks_name['last_quote_date'] = df_stocks_name['last_quote_date'].dt.strftime('%d-%b-%y')
+    
+    # Reorder columns
+    df_stocks_name = df_stocks_name[["symbol", "name", "last_quote_date", "is_prices_uptodate"]]
+    
+    return df_stocks_name, df_macro
+            
+            
+    
+    
--- st_interface.py
+++ st_interface.py
@@ -1,13 +1,97 @@
 import streamlit as st
+import pandas as pd
+
+from src.streamlit.ingest import extract_current_stocks_data
 
 # Function to display the data ingestion page
 def ingest_data_page():
     st.title("Data Ingestion")
-    # Example: Select stocks to ingest
-    selected_stocks = st.multiselect("Select Stocks", ["AAPL", "GOOG", "MSFT", "AMZN", "FB"])
-    if st.button("Ingest Data"):
-        st.write("Data for the following stocks ingested:", selected_stocks)
-        # Add your data ingestion logic here
+    
+    # Call the function to get the stock status DataFrame
+    stock_status_df, macro_status_df = extract_current_stocks_data()
+    
+    # Display the DataFrame in Streamlit
+    st.header("Data Status")
+    
+    # Use st.columns to create a layout with 2 columns
+    col1, col2 = st.columns(2)
+
+    # Display df1 in the first column
+    with col1:
+        st.subheader("Stocks Status")
+        st.dataframe(stock_status_df, use_container_width=True)
+    with col2:
+        st.subheader("Macro Status")
+        st.dataframe(macro_status_df, use_container_width=True)
+        
+    # Display the DataFrame in Streamlit
+    st.header("Ingestion")
+    
+    # Use st.columns to create a layout with 2 columns
+    col1, col2 = st.columns(2)
+    
+    with col1:
+        st.subheader("Stocks Ingestion")
+        df_stocks_ingestion = stock_status_df[["symbol", "name"]]
+        df_stocks_ingestion["to_ingest"] = True
+
+        st.data_editor(
+            df_stocks_ingestion,
+            column_config={
+                "to_ingest": st.column_config.CheckboxColumn(
+                    "To Ingest?",
+                    help="Select the stocks to **ingest** the data",
+                    default=True,
+                )
+            },
+            disabled=["widgets"],
+            hide_index=True,
+        )
+    with col2:
+        st.subheader("Macro Ingestion")
+        df_macro_ingestion = macro_status_df[["indicators"]]
+        df_macro_ingestion["to_ingest"] = True
+
+        st.data_editor(
+            df_macro_ingestion,
+            column_config={
+                "to_ingest": st.column_config.CheckboxColumn(
+                    "To Ingest?",
+                    help="Select the macro to **ingest** the data",
+                    default=True,
+                )
+            },
+            disabled=["widgets"],
+            hide_index=True,
+        )
+        
+    # Custom CSS to inject for styling the buttons
+    st.markdown("""
+        <style>
+        div.stButton > button:first-child {
+            background-color: #4CAF50; 
+            color: white;
+        }
+        div.stButton > button:last-child {
+            background-color: #f44336; 
+            color: white;
+            width: 33%;
+        }
+        </style>""", unsafe_allow_html=True)
+
+    # Layout with two buttons
+    col1, col2 = st.columns([2,1])
+    with col1:
+        ingest_btn = st.button("Ingest")
+    with col2:
+        remove_btn = st.button("Remove 3 last dates")
+
+    # Button logic (as an example)
+    if ingest_btn:
+        st.write("Ingest button clicked")
+
+    if remove_btn:
+        st.write("Remove button clicked")
 
 # Function to display the model building page
 def build_model_page():
